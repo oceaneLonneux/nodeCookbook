1b5981042c8cc8646276a441054ccaf108fbf481
Oceane Lonneux <oceanelonneux@gmail.com>
2018-09-25 11:25:37 +0100

    jsa

---
+++ recipes/default.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook:: node
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+include_recipe("nodejs")
+
+apt_update "update_sources" do
+  action :update
+end
+
+package "nginx" do
+  action :install
+end
+
+service "nginx" do
+  action [:enable, :start]
+end
+
+nodejs_npm "pm2" do
+  action [:install]
+end
+
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  notifies(:restart, 'service[nginx]')
+end
+
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+  notifies(:restart, 'service[nginx]')
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies(:restart, 'service[nginx]')
+end
